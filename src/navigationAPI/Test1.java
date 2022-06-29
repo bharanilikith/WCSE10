22082e394b67c5cd681fe9c82c7ecbcdc283dcb9
QSP <QSP@QSP>
2022-06-29 18:53:05 +0530

    Locators

---
+++ src/navigationAPI/Test1.java
@@ -0,0 +1,30 @@
+package navigationAPI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		
+		driver.navigate().to("https://www.amazon.com/");
+		
+		Thread.sleep(3000);
+		driver.navigate().back();
+		
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		
+		Thread.sleep(3000);
+		driver.close();
+
+	}
+
+}
---
+++ src/windowMangmant/Test0.java
@@ -0,0 +1,28 @@
+package windowMangmant;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) throws InterruptedException  {
+
+WebDriver driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.facebook.com/");
+
+Thread.sleep(3000);
+driver.manage().window().fullscreen();
+
+
+Thread.sleep(3000);
+driver.manage().window().minimize();
+
+
+
+
+
+
+	}
+
+}
