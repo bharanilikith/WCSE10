0e17ad594002470f7d8e9d3f994d0fc47ca717b8
QSP <QSP@QSP>
2022-06-30 19:00:17 +0530

    CSS Selector

--- src/locators/Test2.java
+++ src/locators/Test2.java
@@ -4,17 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Test1 {
+public class Test2 {
 
 	public static void main(String[] args) {
-
 WebDriver driver=new ChromeDriver();
 driver.manage().window().maximize();
-driver.get("https://www.facebook.com/");
+driver.get("https://demo.actitime.com/login.do");
 
-driver.findElement(By.linkText("email")).sendKeys("admin");
-driver.findElement(By.name("pass")).sendKeys("manager");
-driver.findElement(By.name("login")).click();
+driver.findElement(By.linkText("Forgot your password?")).click();
+
 
 	}
 
