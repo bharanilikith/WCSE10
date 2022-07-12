4790a675309282cce49c7734016680a6bcb3d033
QSP <QSP@QSP>
2022-07-12 18:42:15 +0530

    Frames

---
+++ src/actions/Test5.java
@@ -0,0 +1,30 @@
+package actions;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.myntra.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		
+		WebElement dd = driver.findElement(By.xpath("//a[text()='Home & Living']"));
+	Actions a=new Actions(driver);
+	a.moveToElement(dd).perform();
+	
+	driver.findElement(By.xpath("//a[text()='Sofa Covers']")).click();
+	
+	
+	}
+
+}
---
+++ src/dropdown/Test4.java
@@ -0,0 +1,61 @@
+package dropdown;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+
+WebDriver driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.facebook.com/");
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+driver.findElement(By.xpath("//a[text()='Create New Account']")).click();
+WebElement dd = driver.findElement(By.id("month"));
+Select s=new Select(dd);
+s.selectByIndex(0);
+s.selectByValue("3");
+s.selectByVisibleText("May");
+
+List<WebElement> options = s.getOptions();
+System.out.println(options.size());
+
+ArrayList a=new ArrayList<>();
+
+for(WebElement opt:options) {
+	String text = opt.getText();
+	System.out.println(text);
+	a.add(text);
+}
+
+Collections.sort(a);
+
+System.out.println("***********after sorting*******");
+
+for(Object b:a) {
+	System.out.println(b);
+}
+
+driver.close();
+
+
+
+
+
+
+
+
+
+	}
+
+}
