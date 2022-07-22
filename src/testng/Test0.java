87287e92559a5cf67f06719a8e1811c4530549f6
QSP <QSP@QSP>
2022-07-22 21:06:25 +0530

    Testng

---
+++ src/testng/Test0.java
@@ -0,0 +1,13 @@
+package testng;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test0 {
+	
+	@Test
+	public void demo() {
+	Reporter.log("hello java",true);
+	Reporter.log("hello selenium",false);
+	}
+}
---
+++ src/testng/Test1.java
@@ -0,0 +1,23 @@
+package testng;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test(priority = 1,invocationCount = 3)
+	public void calls() {
+		Reporter.log("calls",true);
+	}
+
+	@Test(priority = -1,enabled=false)
+	public void settings() {
+		Reporter.log("settings",true);
+	}
+
+	
+	@Test(priority = -1)
+	public void chats() {
+		Reporter.log("chtas",true);
+	}
+
+}
